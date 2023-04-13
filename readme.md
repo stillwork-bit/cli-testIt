c4354a2463395c232efca99b7652c8fc0df947a3
Tereshin Andrey <Atereshin@inno.tech>
2023-04-13 22:32:20 +0300

    first commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Статья для блога TestIT с примером автоматизации api-автотестов в docker
+
+## CLI plugin TestIt
+
+#### Текст статьи https://docs.google.com/document/d/16wjF1QNjY8l3N2jsXjVKvKgOQcv6KF6MXz37IuyZut0/edit?usp=sharing
+
+#### Ссылка на статью 
---
+++ src/test/java/TestCli.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestCli {
+
+    @Test
+    public void ExportResultTestToTestRunByCliTestItPlugin() {
+        assertEquals("a", "a");
+    }
+}
